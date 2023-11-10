3f4de5b48248188c340293a539911e648fbbb8dd
Kiran Nepali <itsmekiran44@gmai.com>
2023-11-10 20:48:18 +0545

    make sttatic page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Age } from "./components/Age";
+import { BirthDate } from "./components/BirthDate";
+
 function App() {
-  return <>Age calculator</>;
+  return (
+    <>
+      <div className="w-full h-screen mx-auto flex flex-col justify-center items-center gap-6 bg-[#F1EAFF]">
+        <h1 className="font-bold">AGE CALCULATOR</h1>
+        <BirthDate />
+        <Age />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Age.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Age = () => {
+  return <div>Your are 21 years, 4 months and 36 days old.</div>;
+};
